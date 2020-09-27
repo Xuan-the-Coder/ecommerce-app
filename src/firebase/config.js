3116c033dd8d43b9dc1eee3816a46c340e2bcda3
Xuan-the-Coder <wx209dewang@gmail.com>
2020-09-27 17:59:52 -0500

    added firebase config and untils files

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4A-7QH4BpcKHFeehLqvriZgtcuoqjx4w",
+    authDomain: "panda-tea-ecommerce.firebaseapp.com",
+    databaseURL: "https://panda-tea-ecommerce.firebaseio.com",
+    projectId: "panda-tea-ecommerce",
+    storageBucket: "panda-tea-ecommerce.appspot.com",
+    messagingSenderId: "539710940043",
+    appId: "1:539710940043:web:f9d10c07cb9045410e858e"
+};
