f3aa892f83b810d6cc0398328ef326af09224220
Xuan-the-Coder <wx209dewang@gmail.com>
2020-09-27 18:04:16 -0500

    modified page layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
 import NavBar from './components/NavBar.js'
-
+import OutlinedPagination from './components/Pagination'
 import './App.css';
 import PersistentDrawerLeft from './components/Drawer'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import ProductList from "./components/ProductList.js";
+import Product from "./pages/Product.js"
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   const [categories , setCategories] = useState([]);
@@ -17,11 +25,27 @@ function App() {
 
   return (
 
+    <Router>
+
+
+
     <div className="App">
+      <div>
+        <Switch>
+          <Route path="/" component={ProductList} exact />
+          <Route path="/products-list" component={ProductList} />
+          <Route path="/product/:id" component={Product} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+      <div>
+        <OutlinedPagination />
+      </div>
       <div>
         <PersistentDrawerLeft />
       </div>
     </div>
+    </Router>
   );
 }
 
