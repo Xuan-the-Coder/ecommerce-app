f991016f0112f16f873d8f33c034eb4d9b0e4230
Xuan_the_Coder <wx209dewang@gmail.com>
2020-10-06 12:30:40 -0500

    to handle add to cart action

---
+++ src/actions/addAction.js
@@ -0,0 +1,20 @@
+import { ADD_PRODUCT_BASKET } from './types'
+import Axios from "axios";
+
+export const addBasket = (productId) => async (dispatch) => {
+
+    const { data } = await Axios.get(`http://localhost:3000/products/${productId}.json`)
+        console.log("Adding to basket")
+        console.log(productId)
+        dispatch({
+            type: ADD_PRODUCT_BASKET,
+            payload:{
+                id: productId,
+                name: data.name,
+                price: parseFloat(data.price),
+                numbers: 0,
+                inCart: false,
+                image: data.image_url
+            }
+        })
+}
