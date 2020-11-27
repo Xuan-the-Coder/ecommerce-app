b07c30a3290786d35c29fbe09f43d78e0165458a
Xuan-the-Coder <wx209dewang@gmail.com>
2020-11-26 23:30:35 -0600

    modfied about message to include more details about this project

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,36 +1,42 @@
-import React from 'react';
+import React from "react";
 import PersistentDrawerLeft from "./../components/Drawer";
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
 
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 400,
-    },
-  });
+  root: {
+    maxWidth: 400,
+  },
+});
 
 export default function About() {
-    const classes = useStyles();
-    return(
-        <div>
-          <div>
-            <PersistentDrawerLeft />
-          </div>
-          <div>
+  const classes = useStyles();
+  return (
+    <div>
+      <div>
+        <PersistentDrawerLeft />
+      </div>
+      <div>
         <Card className={classes.root} style={{ marginLeft: 530 }}>
-        <CardContent>
+          <CardContent>
             <Typography variant="subtitle1" component="p">
-            This app was originally a final project for my full-stack course. I first built it with Ruby on Rails by followig the MVC pattern. Then I connverted the ROR app to be a back-end JSON API and coded this front-end REACT app by utiling REDUX to manage state, Matterial UI for styling and Firebase to store user info and to handle anthentication. 
+              This app was originally a final project for my full-stack course.
+              I first built it with Ruby on Rails by implementing the MVC
+              pattern. Then I converted it to a back-end JSON API and built this
+              REACT app to act as the front-end piece by utilizing REDUX to
+              manage state, Material UI for styling, and Firebase to store user
+              credentials and to handle authentication.
             </Typography>
-            <br/>
+            <br />
             <Typography variant="subtitle1" component="p">
-            The back-end part of this project is currently hosted on heroku and all images are stored in an AWS S3 bucket. 
+              The back-end piece is hosted in Heroku, and all images are stored
+              in an AWS S3 bucket.
             </Typography>
-        </CardContent>
+          </CardContent>
         </Card>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
